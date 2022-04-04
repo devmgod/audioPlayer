7235c5520bbd79de86ae66f23d403f5427586c45
devguru0112 <devguru0112@gmail.com>
2022-04-03 22:05:55 -0700

    add audio

--- src/tracks.js
+++ src/tracks.js
@@ -1,9 +1,9 @@
 import imgSrc from "./assets/artwork.png";
 import imgSrc2 from "./assets/artwork2.png";
 import imgSrc3 from "./assets/artwork3.png";
-import cali from "./assets/cali-wataboi.m4a";
-import fifty from "./assets/50-tobylane.m4a";
-import iwonder from "./assets/iwonder-dreamheaven.m4a";
+import cali from "https://files.freemusicarchive.org/storage-freemusicarchive-org/music/WFMU/Broke_For_Free/Directionless_EP/Broke_For_Free_-_01_-_Night_Owl.mp3";
+import fifty from "https://files.freemusicarchive.org/storage-freemusicarchive-org/music/no_curator/Tours/Enthusiast/Tours_-_01_-_Enthusiast.mp3";
+import iwonder from "https://files.freemusicarchive.org/storage-freemusicarchive-org/music/ccCommunity/Chad_Crouch/Arps/Chad_Crouch_-_Shipping_Lanes.mp3";
 
 // All of these artists are at https://pixabay.com/music/search/mood/laid%20back/
 const tracks = [
